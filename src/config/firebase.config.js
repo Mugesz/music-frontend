3e5b967510463452e0a9c48520247066b04e2e74
frekerzzzz <kannamugesh135@gmail.com>
2024-01-18 15:51:52 +0530

    auth changed

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,12 +2,13 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_ID,
-  appId: process.env.REACT_APP_FIREBASE_APPI_ID,
+  apiKey: "AIzaSyCTRXa2aLmjVsto47V5jWrYBGOLSUFWAfQ",
+  authDomain: "projectmusicapp-cbe80.firebaseapp.com",
+  databaseURL: "https://projectmusicapp-cbe80-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "projectmusicapp-cbe80",
+  storageBucket: "projectmusicapp-cbe80.appspot.com",
+  messagingSenderId: "650572130720",
+  appId: "1:650572130720:web:4a82d5981f851d80cd5cc5"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
